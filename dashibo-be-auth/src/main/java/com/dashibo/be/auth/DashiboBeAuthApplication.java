977063928e1d1848b8b4efb6f30d6618f9b1a555
Jakker17 <jurosmartin97@gmail.com>
2023-11-03 13:24:56 +0100

    First creatinon of Repo and first backend module

---
+++ dashibo-be-auth/src/main/java/com/dashibo/be/auth/DashiboBeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.dashibo.be.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashiboBeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashiboBeAuthApplication.class, args);
+    }
+
+}
---
+++ dashibo-be-auth/src/test/java/com/dashibo/be/auth/DashiboBeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashibo.be.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashiboBeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
